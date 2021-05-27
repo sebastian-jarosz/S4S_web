f12a4e06bd637c02366503b9a9b4d3c567a4ae3e
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2021-05-27 19:34:46 +0200

    Register models in admin site

--- football_players/admin.py
+++ football_players/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(ApplicationParameters)
+admin.site.register(DominatingFoot)
+admin.site.register(ManagementAgency)
+admin.site.register(Team)
+admin.site.register(Position)
+admin.site.register(Country)
+admin.site.register(League)
+admin.site.register(Season)
+admin.site.register(Queue)
+admin.site.register(Match)
+admin.site.register(Player)
+admin.site.register(PlayerTeam)
+admin.site.register(Goal)
+admin.site.register(Assist)
+admin.site.register(MatchPlayers)

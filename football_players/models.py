ad33a2e69b8b1d075174325ded1f82995c354b03
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2021-05-28 19:30:40 +0200

    Models changes

--- football_players/models.py
+++ football_players/models.py
@@ -62,14 +62,17 @@ class Match(models.Model):
 
 
 class Player(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    position = models.ForeignKey(Position, on_delete=models.DO_NOTHING)
-    foot = models.ForeignKey(DominatingFoot, on_delete=models.DO_NOTHING)
-    agency = models.ForeignKey(ManagementAgency, on_delete=models.DO_NOTHING)
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    date_of_birth = models.DateField(null=True)
+    position = models.ForeignKey(Position, on_delete=models.DO_NOTHING, null=True)
+    foot = models.ForeignKey(DominatingFoot, on_delete=models.DO_NOTHING, null=True)
+    agency = models.ForeignKey(ManagementAgency, on_delete=models.DO_NOTHING, null=True)
     transfermarkt_id = models.IntegerField(unique=True)
     transfermarkt_hyperlink = models.URLField(unique=True, null=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name if self.last_name is not None else self.first_name
 
 
 class PlayerTeam(models.Model):

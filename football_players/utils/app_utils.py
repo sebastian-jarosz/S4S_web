5ff5d9bfd12c93e4733a28ba8616f5de3e73bd1e
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2021-06-03 12:05:26 +0200

    get_pool() added for multithreading purposes

--- football_players/utils/app_utils.py
+++ football_players/utils/app_utils.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from datetime import datetime
 
 
@@ -5,3 +6,7 @@ def parse_transfermarkt_date(transfermarkt_date):
     return (datetime.strptime(transfermarkt_date, "%b %d, %Y").strftime("%Y-%m-%d")) if transfermarkt_date \
                                                                                         is not None else None
 
+
+def get_pool():
+    multiprocessing.set_start_method("fork", force=True)
+    return multiprocessing.Pool(50)

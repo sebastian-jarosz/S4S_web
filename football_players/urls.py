d964ef3e214ae488ae3acf39f8fb19fc3b46d722
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2021-06-20 12:30:30 +0200

    urls change for services

--- football_players/urls.py
+++ football_players/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('country', views.country, name="country"),
-    path('league', views.league, name="league"),
-    path('season', views.season, name="season"),
-    path('queue', views.queue, name="queue"),
-    path('team', views.team, name="team"),
-    path('player', views.player, name="player"),
-    path('player/attributes', views.player_attributes, name="player_attributes"),
-    path('match', views.match, name="match"),
-    path('match/events', views.match_events, name="match_events")
+    path('service/country', views.country, name="country service"),
+    path('service/league', views.league, name="league service"),
+    path('service/season', views.season, name="season service"),
+    path('service/queue', views.queue, name="queue service"),
+    path('service/team', views.team, name="team service"),
+    path('service/player', views.player, name="player service"),
+    path('service/player/attributes', views.player_attributes, name="player attributes service"),
+    path('service/match', views.match, name="match service"),
+    path('service/match/events', views.match_events, name="match events service")
 ]
